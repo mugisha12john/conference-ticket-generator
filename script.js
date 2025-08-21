78f7f0aa29346d7ba43f0198ccfa390e02dce152
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-08-21 16:51:46 +0200

    feat: make background fit in droped area

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ const dropArea = document.getElementById("dropArea");
 const fileInput = document.getElementById("fileInput");
 const imgView = document.getElementById("imgView");
 const dragtext = document.getElementById("drag-text");
-const uploadError = document.getElementById("upload-error")
+const uploadError = document.getElementById("upload-error");
 fileInput.addEventListener("change", uploadImage);
 
 function uploadImage() {
   let image = URL.createObjectURL(fileInput.files[0]);
-  if(image.size < 50 * 1024){
-  imgView.textContent = "";
-  imgView.style.backgroundImage = `url(${image})`;
-  dragtext.textContent = "";
-  const ground = document.createDocumentFragment();
-  const removeBtn = document.createElement("button");
-  removeBtn.className = "removeBtn";
-  removeBtn.textContent = "Remove image";
-  const changeBtn = document.createElement("button");
-  changeBtn.className = "changeBtn";
-  changeBtn.textContent = "Change image";
-  ground.append(removeBtn, changeBtn);
-  dragtext.append(ground);
-  }else{
-    uploadError.style.color = 'red'
-  }
-}
+//   if (image.size/1024 > 580) {
+    const yes = document.querySelector("#imgView img");
+    
+    yes.style.backgroundImage = `url(${image})`;
+    dragtext.textContent = "";
+    const ground = document.createDocumentFragment();
+    const removeBtn = document.createElement("button");
+    removeBtn.className = "removeBtn";
+    removeBtn.textContent = "Remove image";
+    const changeBtn = document.createElement("button");
+    changeBtn.className = "changeBtn";
+    changeBtn.textContent = "Change image";
+    ground.append(removeBtn, changeBtn);
+    dragtext.append(ground);
+  } 
+//   else {
+//     uploadError.textContent =
+//       "File is to large. Please upload image under 500KB.";
+//     uploadError.style.color = "rgba(247, 64, 64, 1)";
+//   }
+
 dropArea.addEventListener("dragover", (e) => {
   e.preventDefault();
 });
