04985088828074dab97c1296e41d9478621a9952
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-08-24 18:06:47 +0200

    feat: check file size

--- script.js
+++ script.js
@@ -3,36 +3,45 @@ const fileInput = document.getElementById("fileInput");
 const imgView = document.getElementById("imgView");
 const dragtext = document.getElementById("drag-text");
 const uploadError = document.getElementById("upload-error");
+
 fileInput.addEventListener("change", uploadImage);
 
 function uploadImage() {
+  let imageUrl = fileInput.files[0];
   let image = URL.createObjectURL(fileInput.files[0]);
-//   if (image.size/1024 > 580) {
-    const yes = document.querySelector("#imgView img");
-    
-    yes.style.backgroundImage = `url(${image})`;
-    dragtext.textContent = "";
-    const ground = document.createDocumentFragment();
-    const removeBtn = document.createElement("button");
-    removeBtn.className = "removeBtn";
-    removeBtn.textContent = "Remove image";
-    const changeBtn = document.createElement("button");
-    changeBtn.className = "changeBtn";
-    changeBtn.textContent = "Change image";
-    ground.append(removeBtn, changeBtn);
-    dragtext.append(ground);
-  } 
-//   else {
-//     uploadError.textContent =
-//       "File is to large. Please upload image under 500KB.";
-//     uploadError.style.color = "rgba(247, 64, 64, 1)";
-//   }
-
+  if (imageUrl.size > 500 * 1024) {
+    uploadError.textContent =
+      "File is to large. Please upload image under 500KB.";
+    uploadError.style.color = "rgba(247, 64, 64, 1)";
+  }else{
+  imgView.textContent = "";
+  imgView.style.backgroundImage = `url(${image})`;
+  dragtext.textContent = "";
+  const ground = document.createDocumentFragment();
+  const removeBtn = document.createElement("button");
+  removeBtn.className = "removeBtn";
+  removeBtn.textContent = "Remove image";
+  removeBtn.setAttribute("id", "removeBtn");
+  const changeBtn = document.createElement("button");
+  changeBtn.className = "changeBtn";
+  changeBtn.textContent = "Change image";
+  changeBtn.setAttribute("id", "changeBtn");
+  ground.append(removeBtn, changeBtn);
+  dragtext.append(ground);
+  }
+}
 dropArea.addEventListener("dragover", (e) => {
   e.preventDefault();
 });
 dropArea.addEventListener("drop", (e) => {
   e.preventDefault();
   fileInput.files = e.dataTransfer.files;
+  uploadImage();
+});
+
+const removeBtn = document.getElementById("removeBtn");
+const changeBtn = document.getElementById("changeBtn");
+changeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
   uploadImage();
 });
